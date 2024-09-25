4bee7fe6ade5f96b8e9d591ed4b23b2da3ce6930
DESKTOP-67MT2K8\user <clerne.celra@gmail.com>
2024-09-25 14:17:43 +0900

    システム：エネミー側仮完成

--- BLAM!!DEMO/Assets/Clerne/Scripts/System/AttackPattern.cs
+++ BLAM!!DEMO/Assets/Clerne/Scripts/System/AttackPattern.cs
@@ -50,7 +50,7 @@ public class AttackPattern : MonoBehaviour
     private bool dodgeSuccesed = false;
     private bool guarded = false;
     private bool guardSuccesed = false;
-    private bool attackEnd = false;
+    private bool dodgeAndGuardFailed = false;
 
     [SerializeField ,ReadOnly]
     private float time;
@@ -60,12 +60,18 @@ public class AttackPattern : MonoBehaviour
 
     Text debugHpText;
     Text debugDodgeText;
+    Text debugGuardText;
+    Text debugDoText;
     [SerializeField]
     int playerMaxHp;
 
     private int playerHp;
 
     private string dodgeTimingStr;
+
+    private string guardTimingStr;
+
+    private string doStr;
 
     void Start()
     {
@@ -76,6 +82,11 @@ public class AttackPattern : MonoBehaviour
         playerHp = playerMaxHp;
 
         debugDodgeText = GameObject.Find("DebugText/Dodge").GetComponent<Text>();
+
+        debugGuardText = GameObject.Find("DebugText/Guard").GetComponent<Text>();
+
+        debugDoText = GameObject.Find("DebugText/Do").GetComponent<Text>();
+
         debugHpText = GameObject.Find("DebugText/HP").GetComponent<Text>();
 
     }
@@ -88,6 +99,12 @@ public class AttackPattern : MonoBehaviour
         PatternController();
 
         DebugText();
+
+        if (dodgeAndGuardFailed)
+        {
+            playerHp -= 100;
+            dodgeAndGuardFailed = false;
+        }
     }
 
     void PatternController()
@@ -96,67 +113,9 @@ public class AttackPattern : MonoBehaviour
         {
             Debug.Log("攻撃開始");
 
-            // デバッグ用
-            dodgeTimingStr = "回避できない";
+            DodgeController();
 
-            if (time <= attackTiming)
-            {
-                if (time >= TimingNum(dodgeTime) && dodged == false)
-                {   // 回避可能時間
-                    if (time >= TimingNum(justDodgeTime))
-                    {   // ジャスト回避可能時間
-                        dodgeTimingStr = "ジャスト回避可能"; // デバッグ用
-
-
-                        if (Input.GetKeyDown(KeyCode.D)) // プレイヤー側のInputにそのうち変える
-                        {
-                            Debug.Log("ジャスト回避成功");
-                            
-
-                            dodgeSuccesed = true;
-                            dodged = true;
-                        }
-
-                    }
-                    else
-                    {   // 通常回避可能時間
-                        dodgeTimingStr = "通常回避可能"; // デバッグ用
-                        if (Input.GetKeyDown(KeyCode.D)) // プレイヤー側のInputにそのうち変える
-                        {
-                            Debug.Log("通常回避成功");
-
-                            dodgeSuccesed = true;
-                            dodged = true;
-
-                        }
-
-                    }
-                }
-                else if (time < TimingNum(dodgeTime))
-                {   // 回避受付時間より早い
-                    dodgeTimingStr = "回避できない"; // デバッグ用
-                    if (Input.GetKeyDown(KeyCode.D)) // プレイヤー側のInputにそのうち変える
-                    {
-                        Debug.Log("回避ボタンを押すのが早すぎ");
-                        Debug.Log("回避失敗");
-
-                        dodged = true;
-
-                    }
-                }
-
-            }
-            else if(!dodgeSuccesed)
-            {
-                Debug.Log("攻撃対処失敗");
-                Debug.Log("HP減らす");
-
-                playerHp -= hpAtk;
-
-                dodgeSuccesed = true;
-            }
-
-
+            GuardController();
 
 
         }
@@ -170,9 +129,146 @@ public class AttackPattern : MonoBehaviour
     {
         debugHpText.text = "エネミーHP:" + hp + " プレイヤーHP:" + playerHp;
 
-        debugDodgeText.text = "経過時間 " + time.ToString("F2") + " " + dodgeTimingStr; 
+        debugDodgeText.text = "経過時間 " + time.ToString("F2") + " " + dodgeTimingStr;
+
+        debugGuardText.text = guardTimingStr;
+
+        debugDoText.text = doStr;
     }
 
+    void DodgeController()
+    {
+        if (time <= attackTiming)
+        {
+            // デバッグ用
+            dodgeTimingStr = "回避できない";
+
+            if (time >= TimingNum(dodgeTime) && dodged == false && guarded == false)
+            {   // 回避可能時間
+                if (time >= TimingNum(justDodgeTime))
+                {   // ジャスト回避可能時間
+                    dodgeTimingStr = "ジャスト回避可能"; // デバッグ用
+
+
+                    if (Input.GetKeyDown(KeyCode.D)) // プレイヤー側のInputにそのうち変える
+                    {
+                        Debug.Log("ジャスト回避成功");
+                        doStr = "ジャスト回避成功"; // デバッグ用
+
+
+                        dodgeSuccesed = true;
+                        dodged = true;
+                    }
+
+                }
+                else
+                {   // 通常回避可能時間
+                    dodgeTimingStr = "通常回避可能"; // デバッグ用
+                    if (Input.GetKeyDown(KeyCode.D)) // プレイヤー側のInputにそのうち変える
+                    {
+                        Debug.Log("通常回避成功");
+                        doStr = "通常回避成功"; // デバッグ用
+
+                        dodgeSuccesed = true;
+                        dodged = true;
+
+                    }
+
+                }
+            }
+            else if (time < TimingNum(dodgeTime))
+            {   // 回避受付時間より早い
+                dodgeTimingStr = "回避できない"; // デバッグ用
+                if (Input.GetKeyDown(KeyCode.D)) // プレイヤー側のInputにそのうち変える
+                {
+                    Debug.Log("回避ボタンを押すのが早すぎ");
+                    Debug.Log("回避失敗");
+                    doStr = "回避失敗(fast)"; // デバッグ用
+
+
+                    dodged = true;
+
+                }
+            }
+
+        }
+        else if (!dodgeSuccesed && !guardSuccesed)
+        {
+            Debug.Log("攻撃対処失敗");
+            Debug.Log("HP減らす");
+
+            dodgeAndGuardFailed = true;
+
+            dodgeSuccesed = true;
+        }
+
+    }
+
+    void GuardController()
+    {
+        if (time <= attackTiming)
+        {
+            // デバッグ用
+            guardTimingStr = "ガードできない";
+
+            if (time >= TimingNum(guardTime) && guarded == false && dodged == false)
+            {   // 回避可能時間
+                if (time >= TimingNum(justGuardTime))
+                {   // ジャスト回避可能時間
+                    guardTimingStr = "ジャストガード可能"; // デバッグ用
+
+
+                    if (Input.GetKeyDown(KeyCode.G)) // プレイヤー側のInputにそのうち変える
+                    {
+                        Debug.Log("ジャストガード成功");
+                        doStr = "ジャストガード成功";
+
+                        guardSuccesed = true;
+                        guarded = true;
+                    }
+
+                }
+                else
+                {   // ガード可能時間
+                    guardTimingStr = "通常ガード可能"; // デバッグ用
+                    if (Input.GetKeyDown(KeyCode.G)) // プレイヤー側のInputにそのうち変える
+                    {
+                        Debug.Log("通常ガード成功");
+                        doStr = "通常ガード成功";
+
+                        guardSuccesed = true;
+                        guarded = true;
+
+                    }
+
+                }
+            }
+            else if (time < TimingNum(guardTime))
+            {   // ガード受付時間より早い
+                guardTimingStr = "ガードできない"; // デバッグ用
+                if (Input.GetKeyDown(KeyCode.G)) // プレイヤー側のInputにそのうち変える
+                {
+                    Debug.Log("ガードボタンを押すのが早すぎ");
+                    Debug.Log("ガード失敗");
+                    doStr = "ガード失敗";
+
+                    guarded = true;
+
+                }
+            }
+
+        }
+        else if (!guardSuccesed && !dodgeSuccesed)
+        {
+            Debug.Log("攻撃対処失敗");
+            Debug.Log("HP減らす");
+
+            dodgeAndGuardFailed = true;
+
+            guardSuccesed = true;   // 仮
+        }
+
+    }
     float TimingNum(float n)
     {
         float timing = attackTiming - n;
