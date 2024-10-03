55e6acf7f8f1bb1f39241d3ec327fbc68905d912
DESKTOP-67MT2K8\user <clerne.celra@gmail.com>
2024-10-03 14:48:12 +0900

    アニメーション仮完成

---
+++ BLAM!!DEMO/Assets/Clerne/Scripts/System/AnimationController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    Player player;
+    Enemy enemy;
+    AttackPattern attackPattern;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AnimInit()
+    {
+        
+    }
+}
