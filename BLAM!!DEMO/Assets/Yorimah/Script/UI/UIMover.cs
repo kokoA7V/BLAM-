20cb3ed7d4aa13d727c6e749908777d95e4d4f36
デスクトップよりま <yoribaseball623@gmail.com>
2024-10-06 19:33:49 +0900

    ポーズ画面を開いた時のアニメーション追加

--- BLAM!!DEMO/Assets/Yorimah/Script/UI/UIMover.cs
+++ BLAM!!DEMO/Assets/Yorimah/Script/UI/UIMover.cs
@@ -1,28 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIMover : MonoBehaviour
 {
     [SerializeField, Header("LightPause")]
-    private GameObject lightGameObject;
+    private Image lightImage;
 
     private Vector3 enterVector3;
     void Start()
     {
-        enterVector3 = lightGameObject.transform.position;
+        enterVector3 = lightImage.transform.position;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnterPause();
     }
 
     // ポーズ画面に入った場合に起動する
     private void EnterPause()
     {
-
+        lightImage.transform.position += new Vector3(1, 0, 0);
     }
 }
