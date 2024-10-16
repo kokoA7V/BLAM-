ed1c5a4b99ea86f6ccab98bf041d85f310cbaaa8
DESKTOP-67MT2K8\user <clerne.celra@gmail.com>
2024-10-16 11:19:04 +0900

    カメラ管理仮

---
+++ BLAM!!DEMO/Assets/Clerne/Scripts/System/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] cam;
+
+    public void CameraChange(int Num)
+    {
+        Reset();
+        cam[Num].SetActive(true);
+    }
+
+    private void Reset()
+    {
+        for(int i = 0; i < cam.Length; ++i)
+        {
+            cam[i].SetActive(false);
+        }
+    }
+}
