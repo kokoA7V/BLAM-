85c3f12a75f3cde847f7f2f6f2c05fbf311fe701
kokoA7V <oktakeru@icloud.com>
2024-10-18 23:01:38 +0900

    Stage１制作中

---
+++ BLAM!!DEMO/Assets/koko/Scripts/PlayerWeaponModelManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponModelManager : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> objList = new List<GameObject>();
+
+    [SerializeField] List<bool> setList = new List<bool>();
+
+    private void Update()
+    {
+    }
+}
